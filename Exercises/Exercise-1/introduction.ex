865226f14b7f0f0f6be8bc31dafd29d46c91c158
Axel Pettersson <axel@pettersson.cc>
2019-01-17 19:30:33 +0100

    Added merge sort

--- Exercises/Exercise-1/introduction.ex
+++ Exercises/Exercise-1/introduction.ex
@@ -65,7 +65,7 @@ defmodule Introduction do
   #-----------------------
 
     # return the n'th element of the list l
-  def nth(n, _list = [head | tail]) do
+  def nth(n, [head | tail]) do
     case n do
       0 -> head
       _ -> nth(n-1, tail)
@@ -73,32 +73,22 @@ defmodule Introduction do
   end
 
   # return the number of elements in the list l
-  def len(_list = [_head | tail]) do
-    case tail do
-      [] -> 1
-      _ -> 1 + len(tail)
-    end
-  end
+  def len([]) do 0 end
+  def len([_head | tail]) do 1 + len(tail) end
 
   # return the sum of all elements in the list l, assume that
   #all elements are integers
-  def sum(_list = [head | tail]) do
-    case tail do
-      [] -> head
-      _ -> head + sum(tail)
-    end
-  end
+  def sum([]) do 0 end
+  def sum([head | tail]) do head + sum(tail) end
 
-    #return a list where all elements are duplicated
-  def duplicate(_list = [head | tail]) do
-    case tail do
-      [] -> [head]
-      _ -> [head | [head | duplicate(tail)]]
-    end
-  end
+  #return a list where all elements are duplicated
+  def duplicate([]) do [] end
+  def duplicate([head | tail]) do [head | [head | duplicate(tail)]] end
 
   #add the element x to the list l if it is not in the list
-  def add(x, list = [head | tail]) do
+  def add(x, []) do [x] end
+  def add(x, list = [head | tail])
+  do
     cond do
       head == x -> list
       tail == [] -> [head | [x]]
@@ -106,12 +96,24 @@ defmodule Introduction do
     end
   end
 
-    #return a list of unique elements in the list l, that is [:a,
-    #:b, :d] are the unique elements in the list [:a, :b, :a, :d, :a,
-    #:b, :b, :a]
 
-    # Function that return true if element is present in list
-    # false if otherwise
+  # remove all occurrences of x in l
+
+  def remove( _ , []) do [] end
+  def remove(x, [head | tail]) do
+    cond do
+      head == x -> remove(x, tail)
+      true -> [head | remove(x, tail)]
+    end
+  end
+
+
+  #return a list of unique elements in the list l, that is [:a,
+  #:b, :d] are the unique elements in the list [:a, :b, :a, :d, :a,
+  #:b, :b, :a]
+
+  # Function that return true if element is present in list
+  # false if otherwise
   def contains(element, list) do
     case list do
       [] -> false
@@ -127,32 +129,26 @@ defmodule Introduction do
   # of unique elements
   def unique([], unique_list) do unique_list end
 
-    # Check if the the next element is present in our unique list
-    # If present -> move on
-    # If not present -> add to unique list and move on
-  def unique(_list = [head | tail], unique_list) do
+  # Check if the the next element is present in our unique list
+  # If present -> move on
+  # If not present -> add to unique list and move on
+  def unique([head | tail], unique_list) do
     case contains(head, unique_list) do
       true -> unique(tail, unique_list)
       false -> unique(tail, unique_list ++ [head])
     end
   end
 
-    #pack(l): return a list containing lists of equal elements, [:a, :a, :b,
-    #:c, :b, :a, :c] should return [[:a, :a, :a], [:b, :b], [:c, :c]]
-
+  #pack(l): return a list containing lists of equal elements, [:a, :a, :b,
+  #:c, :b, :a, :c] should return [[:a, :a, :a], [:b, :b], [:c, :c]]
   def pack(_list) do
 
   end
 
   #return a list where the order of elements is reversed
-  def reverse(l) do
-    [head | tail] = l
-    case tail do
-      [] -> [head]
-      _ ->  reverse(tail) ++ [head]
-    end
-  end
+  def reverse([]) do [] end
 
+  def reverse([head | tail]) do reverse(tail) ++ [head] end
 
   #-----------------------
   # Sorting
@@ -180,4 +176,30 @@ defmodule Introduction do
       [head | tail] -> insert(head, sorted_list) |> isort(tail)
     end
   end
+
+
+  # Merge sorted
+  def msort([ head | [] ]) do [head] end
+  def msort(list) do
+    {list1, list2} = msplit(list, [], [])
+    merge(msort(list1),msort(list2))
+  end
+
+  # Megre two sublists
+  def merge(list1, []) do list1 end
+  def merge([], list2) do list2 end
+  def merge(list1 = [head1 | tail1], list2 = [head2 | tail2]) do
+    cond do
+      head1 < head2 -> [head1 | merge(tail1, list2)]
+      true -> [head2 | merge(list1, tail2)]
+    end
+  end
+
+  def msplit([],list1, list2) do
+    {list1, list2}
+  end
+  def msplit([head | tail],list1, list2) do
+    msplit(tail, [head | list2], list1)
+  end
+
 end
