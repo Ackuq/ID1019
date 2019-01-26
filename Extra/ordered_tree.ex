771230c90ef673206668774ec6b30882eab5f341
Axel Pettersson <axel@pettersson.cc>
2019-01-26 13:29:39 +0100

    Fixed delete and minValueNode functions.

--- Extra/ordered_tree.ex
+++ Extra/ordered_tree.ex
@@ -2,10 +2,12 @@ defmodule OrderedTree do
 
   # https://people.kth.se/~johanmon/courses/id1019/lectures/trees.pdf
 
+  # Create new tree a key and a value
   def init(key, val) do
     {:node, key, val, :nil, :nil}
   end
 
+  # Check if key exists in tree
   def member(_, :nil) do :no end
   def member(key, {:node, key, _, _, _}) do :yes end
   def member(key, {:node, k, _, left, right}) do
@@ -17,7 +19,8 @@ defmodule OrderedTree do
   end
 
 
-  # Add element to tree
+  # Add element to tree, traverse into the tree until you
+  # find an empty spot
   def insert(key, value, :nil) do {:node, key, value, :nil, :nil} end
   def insert(key, value, {:node, k, v, left, right}) do
     if key < k do
@@ -28,8 +31,9 @@ defmodule OrderedTree do
   end
 
   # Get the minimum node from root
-  def minValueNode({:node, key, value, :nil, _})do
-    [key, value]
+  def minValueNode({:node, key, value, :nil, _}) do
+    # return a tuple with the key and value of the minimum value node
+    {key, value}
   end
   def minValueNode({:node, _,_, left, _}) do
     minValueNode(left)
@@ -39,9 +43,10 @@ defmodule OrderedTree do
   def delete(key, {:node, key, _, :nil, :nil}) do :nil end
   def delete(key, {:node, key, _, :nil, right}) do right end
   def delete(key, {:node, key, _, left, :nil}) do left end
-  # Node with two children: Get the inorder successor
+
+  # Node with two children: Get the inorder successor of the right node
   def delete(key, {:node, key, _, left, right }) do
-    [k | [v | _]] = minValueNode(right)
+    {k, v} = minValueNode(right)
     {:node, k, v,  left, delete(k, right)}
   end
   def delete(key, {:node, k, v, left, right}) do
